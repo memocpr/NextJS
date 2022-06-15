27454a4f35985a05cd0380768194e67029816405
Copur Mehmet, INI-DNA-NB <mehmet.copur@swisscom.com>
2022-06-15 11:27:14 +0200

    getStaticPath not working

--- Next/pre-render/pages/[pid].jsx
+++ Next/pre-render/pages/[pid].jsx
@@ -6,10 +6,6 @@ import path from 'path';
 export default function ProductDetailPage(props){
 	const {loadedProduct}=props;
 
-	if(!loadedProduct){
-		return <p>Loading...</p>
-	}
-
 	return (
 		<Fragment>
 			<h1>{loadedProduct.title}</h1>
@@ -18,14 +14,18 @@ export default function ProductDetailPage(props){
 	);
 }
 
+const getData=async()=>{
+	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
+ 	const jsonData =await fs.readFile(filePath);
+	const data = JSON.parse(jsonData);
+	return data;
+}
+
 export const getStaticProps = async(context)=>{
 	const {params} = context;
 	const productId = params.pid;
 
-	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json')
- 	const jsonData =await fs.readFile(filePath);
-	const data = JSON.parse(jsonData);
-
+	const data =await getData();
 	const product = data.products.find(p => p.id===productId);
 
 	return {
@@ -37,11 +37,23 @@ export const getStaticProps = async(context)=>{
 
 export const getStaticPaths = async()=>{
 
+	const data=await getData();
+
+	// collect ids in an array
+	const ids=data.products.map(p => p.id);
+
+	console.log('ids', ids);
+
+	// select id matching with params id
+	const pathWithParams=ids.map(id => ({params:{pid:id}}));
+
+	console.log('pathWithParams', pathWithParams);
+
 	return {
 		paths:[
-			{params:{pid:'p1'}}
+			{params:pathWithParams}
 		],
-		fallback:true // only pre-generate paths indicated above
+		fallback:false
 	}
 
 }
